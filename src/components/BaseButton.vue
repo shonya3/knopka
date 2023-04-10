ff0ac00be0b2768fd06294a09848eefa80a59368
shonya3 <poeshonya3@gmail.com>
2023-04-10 18:57:51 +0300

    add default props to basebutton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+export interface BaseButtonProps {
+	disabled: boolean;
+	type: 'submit' | 'button';
+}
+
+withDefaults(defineProps<BaseButtonProps>(), { disabled: false, type: 'button' });
+</script>
+
 <template>
 	<button type="button"><slot /></button>
 </template>
