0a2537095ac5c37e0248bdfe74ae08ed9bba0c2a
shonya3 <poeshonya3@gmail.com>
2023-04-23 18:54:02 +0300

    bind props to basebutto

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 export interface BaseButtonProps {
-	disabled: boolean;
-	type: 'submit' | 'button';
+	disabled?: boolean;
+	type?: 'submit' | 'button';
 }
 
 withDefaults(defineProps<BaseButtonProps>(), { disabled: false, type: 'button' });
 </script>
 
 <template>
-	<button type="button"><slot /></button>
+	<button :type="type" :disabled="disabled"><slot /></button>
 </template>
 
 <style></style>
